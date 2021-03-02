c19e77415f00ce0e80ad2ead2c9dc3e4c4fadc1d
unknown <lucas_e_93@hotmail.com>
2021-03-02 19:47:16 -0300

    asd

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,10 +3,8 @@ const path = require('path')
 
 const eventController = {
   addEvent: async (req, res) => {
-    console.log(req.body.title)
-    console.log(req.files)
+
     const { title, descripcion, category, dateEvent } = req.body
-    //const { name, profilePicture, _id } = req.user
     const file = req.files.file
     const articlePictureUbicacion = `/assets/eventsPics/${file.md5}.jpg`
     const articleSave = new Event({
@@ -15,11 +13,6 @@ const eventController = {
       picture: articlePictureUbicacion,
       category,
       dateEvent
-      // author: {
-      //   name,
-      //   profilePicture,
-      //   idUser: _id
-      // },
     })
 
     file.mv(path.join(__dirname, `../frontend/public/assets/eventsPics/${file.md5}.jpg`), error => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ function App(props) {
         <Route path="/menu" component={Menu} />
         <Route path="/contact" component={Contact} />
         <Route path="/profile" component={Profile}/>
+        <Route path="/createEvent" component={CreateEvent} />
         <Redirect to="/" />
       </Switch>
     } else {
@@ -67,7 +68,6 @@ function App(props) {
   return (
     <>
     <h1>Cocoliche</h1>
-    <CreateEvent />
     <Router>
         <ScrollToTop>
           {routes}
