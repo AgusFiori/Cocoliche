205dd43dd282fc49634198c7cb0917c82b953bb6
roram <ces.ram.franco@gmail.com>
2021-03-02 13:15:23 -0300

    Mis cambios

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-a
+const express = require('express')
+const router = express.Router()
+// require('../config/passport')
+// const passport = require('passport')
+
+// const productController = require('../controllers/productController')
+
+// router.route('/addProduct').post(productController.addProduct)
+
+
+
+module.exports = router
