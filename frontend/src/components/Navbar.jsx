29c19cad56ea09df7499c20098799239060a3b72
Matias Gomez <mfgomez.810@gmail.com>
2021-03-04 15:48:48 -0300

    Estilos Admin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,14 +5,17 @@ import {IoFastFoodOutline} from 'react-icons/io5'
 import { GoSignOut } from "react-icons/go";
 import { connect } from 'react-redux'
 import logo from '../assets/cocoliche-logo.png'
+import blackboard from '../assets/blackboard.jpg'
+
+
 
 const Navbar = (props) => {
   if (props.loggedUser === null) {
     var links = <>
-          <NavLink to="/login" className="text-decoration-none m-2">
+          <NavLink to="/login" className="text-decoration-none">
             <span className="h4" >Ingresar</span>
           </NavLink>
-          <NavLink to="/register" className="text-decoration-none m-2">
+          <NavLink to="/register" className="text-decoration-none">
             <span className="h4">Registrarse</span>
           </NavLink>
     </>
@@ -28,7 +31,7 @@ const Navbar = (props) => {
             <NavLink to="/">
                 <GoSignOut className="logOut" onClick={() => props.logoutUser()} />
             </NavLink>
-            <NavLink to="/admin" className="text-decoration-none m-2">
+            <NavLink to="/admin" className="text-decoration-none">
                 <span className="h4">Administracion</span>
             </NavLink>
         </>
@@ -47,22 +50,21 @@ const Navbar = (props) => {
             </> }
     }
   return (
-      <div className="d-flex flex-column">
+      <div className="d-flex flex-column position-fixed nav-coco" style={{backgroundImage: `url(${blackboard})`}}>
           <div className="logo mt-4 mb-2" style={{backgroundImage: `url(${logo})`}}>
           </div>
-          <div className="menu mx-auto d-flex flex-column">
+          <div className="menu d-flex flex-column">
             {links}
-            
-            <NavLink to="/menu" className="text-decoration-none m-2">
+            <NavLink to="/menu" className="text-decoration-none">
               <span className="h4">Menú</span>
             </NavLink>
-            <NavLink to="/cart" className="text-decoration-none m-2">
+            <NavLink to="/cart" className="text-decoration-none">
               <span className="h4 d-flex align-items-center">Mi pedido <IoFastFoodOutline/></span>
             </NavLink>
-            <NavLink to="/calendar" className="text-decoration-none m-2">
+            <NavLink to="/calendar" className="text-decoration-none">
               <span className="h4">Eventos</span>
             </NavLink>
-            <NavLink to="/contact" className="text-decoration-none m-2">
+            <NavLink to="/contact" className="text-decoration-none">
               <span className="h4">Contacto</span>
             </NavLink>
           </div>
