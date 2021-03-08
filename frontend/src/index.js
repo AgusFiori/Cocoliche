55190e717bbb7d2b7e1ba3b7ab555880798265c7
unknown <lucas_e_93@hotmail.com>
2021-03-08 10:32:33 -0300

    firebase web

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,12 +10,12 @@ import firebase from 'firebase'
 // Initialize Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyA39V7iOwT-_TJ4NwP1P0IfjKxFEhoE6q4",
-    authDomain: "cocoliche-resto-bar.firebaseapp.com",
-    projectId: "cocoliche-resto-bar",
-    storageBucket: "cocoliche-resto-bar.appspot.com",
-    messagingSenderId: "909495572810",
-    appId: "1:909495572810:web:40ece9ce94e5dca61049b3",
-    measurementId: "G-EV08JYJD0C"
+  authDomain: "cocoliche-resto-bar.firebaseapp.com",
+  projectId: "cocoliche-resto-bar",
+  storageBucket: "cocoliche-resto-bar.appspot.com",
+  messagingSenderId: "909495572810",
+  appId: "1:909495572810:web:e4e004d1f31849f31049b3",
+  measurementId: "G-NJNYKC3MGR"
 });
 
 ReactDOM.render(
