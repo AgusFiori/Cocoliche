42fb78b64cba924725c9318de3d1e7ae0b1716e0
unknown <lucas_e_93@hotmail.com>
2021-03-07 21:32:11 -0300

    asd

--- frontend/src/components/SubCategory.jsx
+++ frontend/src/components/SubCategory.jsx
@@ -25,27 +25,14 @@ const SubCategory = (props) => {
     props.addToCart(productToAdd);
   };
 
+  
   return (
     <>
       <h3>{props.sub.subcategory}</h3>
       <h3>{props.sub.subcategoryPrice}</h3>
       <select name="subcategory" onChange={handleChange} defaultValue="default">
         <option value="default">Elegi cantidad</option>
-        {[...Array(props.sub.subcategoryStock)].map(() => {
-          return (
-            <option
-              value={JSON.stringify({
-                subcategory: props.sub.subcategory,
-                price: props.sub.subcategoryPrice,
-                subcategoryId: props.sub._id,
-                qty,
-                stock: props.sub.subcategoryStock,
-              })}
-            >
-              {qty++}
-            </option>
-          );
-        })}
+        
       </select>
       <button onClick={addToCart}>Agregar</button>
     </>
